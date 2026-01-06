faa715b3b723983eba99e100b44b41a2729ad1d6
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-07 00:27:33 +0800

    feat(uploadToPinata): connected to mintProduct function

--- services/supplier.js
+++ services/supplier.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import callApi from "@/config/api";
+import { pinataApiKey, pinataApiSecret } from "../config"
 
 const ROOT_API = "https://tblo-server.vercel.app";
 const API_VERSION = "api/v1";
@@ -19,4 +20,29 @@ export async function registeredSuppliers() {
         url,
         method: "GET",
     });
+}
+
+export async function uploadToPinata(metadata, tokenId) {
+  try {
+    const res = await axios.post(
+      "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+      {
+        pinataContent: metadata,
+        pinataMetadata: {
+          name: `metadata-${tokenId}.json`,
+        }
+      },
+      {
+        headers: {
+          pinata_api_key: pinataApiKey,
+          pinata_secret_api_key: pinataApiSecret,
+        }
+      }
+    );
+
+    return `ipfs://${res.data.IpfsHash}`;
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    throw err;
+  }
 }
