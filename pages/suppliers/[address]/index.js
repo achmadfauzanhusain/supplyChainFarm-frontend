84e3c2f3112deeb973138344066bd038fecee299
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-08 00:50:59 +0800

    feat(fetchProduct): fetch product based p.supplier === router.query.address

--- pages/suppliers/[address]/index.js
+++ pages/suppliers/[address]/index.js
@@ -1,17 +1,72 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from 'next/navigation'
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router'
 
 import { Urbanist } from "next/font/google"
-
 const urbanist = Urbanist({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800"] })
 
+import { BrowserProvider, Contract } from "ethers";
+import config from "@/config.json";
+import SupplyChainNFT from "../../../abis/SupplyChainNFT.json";
+
 const Supplier = () => {
+    const [contract, setContract] = useState(null);
+    const [products, setProducts] = useState()
+
     const router = useRouter()
+    const { address } = router.query
+    
+    const loadBlockchainData = async() => {
+        const provider = new BrowserProvider(window.ethereum);
+        const network = await provider.getNetwork();
+        const supplyChainNFT = new Contract(
+            config[network.chainId].SupplyChainNFT.address,
+            SupplyChainNFT,
+            provider
+        );
+        setContract(supplyChainNFT);
+    }
+
+    const fetchProduct = async() => {
+        if(!contract) return
+        const nextTokenId = await contract.nextTokenId()
+        const lastTokenId = Number(nextTokenId) - 1
+
+        const items = []
+        for(let tokenId = 1; tokenId <= lastTokenId; tokenId++) { 
+            const p = await contract.products(tokenId)
+
+            if(p.supplier === address) {
+                items.push({
+                    tokenId,
+                    name: p.name,
+                    origin: p.origin,
+                    batch: p.batch_number,
+                    quantity_kg: p.quantity_kg.toString(),
+                    metadataURI: p.metadataURI,
+                    status: p.currentStatus,
+                    supplier: p.supplier,
+                    createdAt: Number(p.createdAt),
+                });
+            }
+        }
+        setProducts(items)
+    }
+
+    useEffect(() => {
+        loadBlockchainData();
+    }, []);
+
+    useEffect(() => {
+        if (!contract) return;
+        fetchProduct();
+    }, [contract]);
+    console.log(products)
     return (
         <div className="mt-10 md:mt-18 px-5 md:px-8 lg:px-18">
             <button className="cursor-pointer" onClick={() => router.back()}>
-                <Image className="w-[25px] md:w-[30px]" src="/icon/back.png" width={30} height={0} />
+                <Image className="w-[25px] md:w-[30px]" src="/icon/back.png" width={30} height={0} alt="" />
             </button>
 
             <h1 className={`${urbanist.className} text-3xl sm:text-4xl md:text-5xl font-bold mt-8`}>PT KOPI INDONESIA</h1>
