3c69ebff24862e823fe7d8ae6c8891e16f600d9a
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-09 00:20:42 +0800

    perf(skeleton): add skeleton loading on product & metadata

--- pages/suppliers/[address]/[id]/index.js
+++ pages/suppliers/[address]/[id]/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/router'
 
@@ -18,8 +19,11 @@ import 'react-toastify/dist/ReactToastify.css';
 const DetailProduct = () => {
     const [contract, setContract] = useState(null)
     const [product, setProduct] = useState({})
-    const [tokenId, setTokenId] = useState()
+    const [tokenId, setTokenId] = useState(null)
     const [metadata, setMetadata] = useState(null)
+
+    const [loadingProduct, setLoadingProduct] = useState(true)
+    const [loadingMetadata, setLoadingMetadata] = useState(true)
 
     const router = useRouter()
     const { id } = router.query
@@ -36,30 +40,46 @@ const DetailProduct = () => {
     }
 
     const fetchDetailProduct = async() => {
-        const nextTokenId = await contract.nextTokenId()
-        const tokenId = Number(nextTokenId) - 1
-        const product = await contract.products(id)
-        setProduct(product)
-        setTokenId(tokenId)
+        try {
+            const nextTokenId = await contract.nextTokenId()
+            const lastTokenId = Number(nextTokenId) - 1
+            const product = await contract.products(id)
+
+            setProduct(product)
+            setTokenId(lastTokenId)
+        } catch (error) {
+            toast.error(error)
+        } finally {
+            setLoadingProduct(false)
+        }
     }
 
     useEffect(() => {
         loadBlockchainData()
+    }, [])
 
-        if (!contract || !router.isReady) return;
+    useEffect(() => {
+        if (!contract || !router.isReady) return
         fetchDetailProduct()
     }, [contract, router.isReady])
 
     useEffect(() => {
         if(!product.metadataURI) return
+
         const fetchMetadata = async() => {
             try {
-                
+                const url = await ipfsToHttp(product.metadataURI)
+                const res = await axios.get(url)
+                setMetadata(res.data)
             } catch (error) {
                 toast.error(error)
+            } finally {
+                setLoadingMetadata(false)
             }
         }
+        fetchMetadata()
     }, [product.metadataURI])
+
     return (
         <div className="mt-10 md:mt-18 px-2 sm:px-8 lg:px-18 pb-8">
             <button className="cursor-pointer" onClick={() => router.back()}>
@@ -67,58 +87,111 @@ const DetailProduct = () => {
             </button>
 
             <div className="flex justify-between items-center mt-8">
-                <h1 className={`${urbanist.className} text-3xl sm:text-4xl md:text-5xl font-bold`}>{product?.name}</h1>
+                <h1 className={`${urbanist.className} text-3xl sm:text-4xl md:text-5xl font-bold`}>
+                    {loadingProduct ? (
+                        <div className="h-10 w-48 bg-gray-300 rounded-md animate-pulse"></div>
+                    ) : (
+                        product?.name
+                    )}
+                </h1>
 
                 <div>
                     <p className="text-xs sm:text-sm opacity-75 font-semibold">tokenId</p>
-                    <h2 className="text-xl sm:text-2xl md:text-3xl font-bold">#{tokenId}</h2>
+                    {loadingProduct ? (
+                        <div className="h-8 w-16 bg-gray-300 rounded-md animate-pulse"></div>
+                    ) : (
+                        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold">#{tokenId}</h2>
+                    )}
                 </div>
             </div>
+
             <div className="flex justify-between items-center mt-4">
                 <div>
                     <p className="text-xs sm:text-sm opacity-75 font-semibold">batch</p>
-                    <h2 className="text-xl sm:text-2xl md:text-3xl font-bold">#{product?.batch_number}</h2>
+                    {loadingProduct ? (
+                        <div className="h-8 w-20 bg-gray-300 rounded-md animate-pulse"></div>
+                    ) : (
+                        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold">
+                            #{product?.batch_number}
+                        </h2>
+                    )}
                 </div>
 
-                <div>
-                <Link href="/suppliers/0x29f19a33c3af612cb5248d2e208b1113d0898e5b/3/edit-status" className="relative cursor-pointer transition-all duration-300 px-3 sm:px-6 py-2 rounded-lg text-white 
-                            bg-linear-to-r from-[#0D6EFD] to-blue-600
-                            shadow-inner overflow-hidden
-                            hover:from-blue-400 hover:to-blue-500
-                            inset-shadow-sm inset-shadow-white/5"
+                <Link
+                    href={`/suppliers/${product?.supplier}/${id}/edit-status`}
+                    className="relative cursor-pointer transition-all duration-300 px-3 sm:px-6 py-2 rounded-lg text-white
+                    bg-linear-to-r from-[#0D6EFD] to-blue-600
+                    shadow-inner overflow-hidden
+                    hover:from-blue-400 hover:to-blue-500
+                    inset-shadow-sm inset-shadow-white/5"
                 >
                     <span className="relative z-10 text-xs">edit status</span>
                     <span className="absolute inset-0 bg-white opacity-20 blur-md rounded-lg"></span>
                 </Link>
-                </div>
             </div>
 
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-3 mt-5">
                 <div className="bg-[#BEE3F8]/25 py-3 md:py-4 px-2 md:px-3 rounded-lg">
                     <h2 className="text-sm md:text-base font-bold">quantity</h2>
-                    <p className="text-xs font-medium opacity-75">{Number(product.quantity_kg)}kg</p>
+                    {loadingProduct ? (
+                        <div className="h-4 w-16 bg-gray-300 rounded animate-pulse mt-1"></div>
+                    ) : (
+                        <p className="text-xs font-medium opacity-75">{Number(product.quantity_kg)}kg</p>
+                    )}
                 </div>
+
                 <div className="bg-[#BEE3F8]/25 py-3 md:py-4 px-2 md:px-3 rounded-lg">
                     <h2 className="text-sm md:text-base font-bold">origin</h2>
-                    <p className="text-xs font-medium opacity-75">{product?.origin}</p>
+                    {loadingProduct ? (
+                        <div className="h-4 w-20 bg-gray-300 rounded animate-pulse mt-1"></div>
+                    ) : (
+                        <p className="text-xs font-medium opacity-75">{product?.origin}</p>
+                    )}
                 </div>
+
                 <div className="bg-[#BEE3F8]/25 py-3 md:py-4 px-2 md:px-3 rounded-lg">
                     <h2 className="text-sm md:text-base font-bold">production date</h2>
-                    <p className="text-xs font-medium opacity-75">2025 - 10 - 25</p>
+                    {loadingMetadata ? (
+                        <div className="h-4 w-24 bg-gray-300 rounded animate-pulse mt-1"></div>
+                    ) : (
+                        <p className="text-xs font-medium opacity-75">{metadata?.productionDate}</p>
+                    )}
                 </div>
+
                 <div className="bg-[#BEE3F8]/25 py-3 md:py-4 px-2 md:px-3 rounded-lg">
                     <h2 className="text-sm md:text-base font-bold">current status</h2>
-                    <p className="text-xs font-medium opacity-75">{product?.currentStatus}</p>
+                    {loadingProduct ? (
+                        <div className="h-4 w-20 bg-gray-300 rounded animate-pulse mt-1"></div>
+                    ) : (
+                        <p className="text-xs font-medium opacity-75">{product?.currentStatus}</p>
+                    )}
                 </div>
+
                 <div className="bg-[#BEE3F8]/25 py-3 md:py-4 px-2 md:px-3 rounded-lg">
                     <h2 className="text-sm md:text-base font-bold">address supplier</h2>
-                    <p className="text-xs font-medium opacity-75">{product?.supplier?.slice(0, 6) + '...' + product?.supplier?.slice(38, 42)}</p>
+                    {loadingProduct ? (
+                        <div className="h-4 w-28 bg-gray-300 rounded animate-pulse mt-1"></div>
+                    ) : (
+                        <p className="text-xs font-medium opacity-75">
+                            {product?.supplier?.slice(0, 6) + "..." + product?.supplier?.slice(38, 42)}
+                        </p>
+                    )}
                 </div>
             </div>
 
             <div className="mt-14">
                 <h2 className="text-sm font-semibold">Description</h2>
-                <p className="text-xs opacity-75 mt-1 font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                {loadingMetadata ? (
+                    <div className="mt-2 space-y-2 animate-pulse">
+                        <div className="h-3 w-full bg-gray-300 rounded"></div>
+                        <div className="h-3 w-5/6 bg-gray-300 rounded"></div>
+                        <div className="h-3 w-4/6 bg-gray-300 rounded"></div>
+                    </div>
+                ) : (
+                    <p className="text-xs opacity-75 mt-1 font-semibold">
+                        {metadata?.description}
+                    </p>
+                )}
             </div>
         </div>
     )
