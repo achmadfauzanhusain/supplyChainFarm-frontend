3c170670a120ea7c7adc1937fb04daafe827578c
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-11 20:23:34 +0800

    perf(ui): remove dark class

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,14 @@
 import Head from "next/head"
 import { Poppins } from "next/font/google";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800"] });
-import { ThemeProvider } from "next-themes"
 
 const Layout = ({ children }) => {
     return (
         <>
             <Head>
                 <title>TBLO</title>
-                <meta name="color-scheme" content="light"></meta>
             </Head>
-            <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-                <main className={`${poppins.className} px-2 sm:px-4 md:px-12 lg:px-20 mt-8 mx-auto`}>{children}</main>
-            </ThemeProvider>
+            <main className={`${poppins.className} px-2 sm:px-4 md:px-12 lg:px-20 mt-8 mx-auto`}>{children}</main>
         </>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,7 @@ import Layout from "@/components/layout";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useEffect } from "react";
-
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-  document.documentElement.classList.remove("dark")
-}, [])
   return (
     <Layout>
       <Component {...pageProps} />
