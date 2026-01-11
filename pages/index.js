176c1ca54986f2c1aec6bec51c074b2cb599e161
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-11 21:35:28 +0800

    style(button register): change to Link

--- pages/index.js
+++ pages/index.js
@@ -68,14 +68,14 @@ export default function Home() {
           <h1 className={`${urbanist.className} text-4xl md:text-5xl font-bold bg-linear-to-r from-[#0D6EFD] bg-clip-text inline-block to-white text-transparent`}>You Have a <br className="sm:hidden" /> Bussiness?</h1>
           <p className="px-8 sm:px-0 mt-4 text-sm sm:text-base md:text-lg font-medium opacity-75">Do you have a company or are you a supplier? You <br /> can make your product <span className="text-[#38B2AC] opacity-100">Supply Chain</span> transparent.</p>
           
-          <button className="relative cursor-pointer transition-all duration-300 px-6 py-3 rounded-lg text-white
+          <Link href="/register" className="relative cursor-pointer transition-all duration-300 px-6 py-3 rounded-lg text-white
             bg-linear-to-r from-[#0D6EFD] to-blue-600
             shadow-inner overflow-hidden
             hover:from-blue-400 hover:to-blue-500
             inset-shadow-sm inset-shadow-white/50 mt-6 text-xs md:text-sm">
             <span className="relative z-10">Register Now</span>
             <span className="absolute inset-0 bg-white opacity-20 blur-md translate-x-3 translate-y-3 rounded-lg"></span>
-          </button>
+          </Link>
         </div>
 
         {/* footer */}
