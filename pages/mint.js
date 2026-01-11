b37bd69498b9a49f2c7459544f16551dabdf92a2
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-11 20:02:58 +0800

    perf(ux): only signer can view minting input

--- pages/mint.js
+++ pages/mint.js
@@ -86,20 +86,31 @@ const Mint = () => {
             <div className="mt-10">
                 <h1 className={`${urbanist.className} text-2xl sm:text-3xl md:text-4xl font-[1000]`}>Mint New Product</h1>
 
-                <div className="mt-10 flex flex-col gap-12">
-                    <input value={name} onChange={(event) => setName(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter product name" />
-                    <input value={origin} onChange={(event) => setOrigin(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter origin name: Toraja, Sulawesi Selatan" />
-                    <input value={batchNumber} onChange={(event) => setBatchNumber(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter batch number" />
-                    <input value={quantityKg} onChange={(event) => setQuantityKg(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter quantity per kg" />
-                    <input value={productionDate} onChange={(event) => setProductionDate(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Production date: YYYY-MM-DD" />
-                    <input value={description} onChange={(event) => setDescription(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter product description" />
-                </div>
-                <button 
-                    onClick={mintProduct}
-                    className="relative cursor-pointer transition-all duration-300 py-3 md:py-4 rounded-lg text-white bg-linear-to-r from-[#0D6EFD] to-blue-600 shadow-inner overflow-hidden hover:from-blue-400 hover:to-blue-500 inset-shadow-sm inset-shadow-white/5 w-full mt-8 text-xs md:text-sm font-semibold">
-                    <span className="relative z-10">Mint Product</span>
-                    <span className="absolute inset-0 bg-white opacity-20 blur-md translate-x-3 translate-y-3 rounded-lg"></span>
-                </button>
+                {/* jika signer tidak ada jangan tampilkan ini! */}
+                {signer && (
+                    <>
+                        <div className="mt-10 flex flex-col gap-12">
+                            <input value={name} onChange={(event) => setName(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter product name" />
+                            <input value={origin} onChange={(event) => setOrigin(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter origin name: Toraja, Sulawesi Selatan" />
+                            <input value={batchNumber} onChange={(event) => setBatchNumber(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter batch number" />
+                            <input value={quantityKg} onChange={(event) => setQuantityKg(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter quantity per kg" />
+                            <input value={productionDate} onChange={(event) => setProductionDate(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Production date: YYYY-MM-DD" />
+                            <input value={description} onChange={(event) => setDescription(event.target.value)} className="w-full px-3 md:px-4 py-4 text-sm sm:text-base md:text-lg outline-none font-bold border-b" placeholder="Enter product description" />
+                        </div>
+
+                        <button 
+                            onClick={mintProduct}
+                            className="relative cursor-pointer transition-all duration-300 py-3 md:py-4 rounded-lg text-white bg-linear-to-r from-[#0D6EFD] to-blue-600 shadow-inner overflow-hidden hover:from-blue-400 hover:to-blue-500 inset-shadow-sm inset-shadow-white/5 w-full mt-8 text-xs md:text-sm font-semibold">
+                            <span className="relative z-10">Mint Product</span>
+                            <span className="absolute inset-0 bg-white opacity-20 blur-md translate-x-3 translate-y-3 rounded-lg"></span>
+                        </button>
+                    </>
+                )}
+                {!signer && (
+                    <p className="mt-10 text-sm opacity-70 font-semibold">
+                        Register become supplier uf you want to minting product
+                    </p>
+                )}
             </div>
         </div>
     )
